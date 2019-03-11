ac35740ec3b68b4f3a5c10b1dcc10e22a6a91975
S.Vasilev <s.vasilev9169@gmail.com>
2019-03-12 00:26:08 +0200

    Player Movement,Waypoint

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+    [SerializeField]
+    private float delay = 5f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadNextScene", delay);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In mPerSec")]
+    [SerializeField] float speed = 50f;
+
+    [Tooltip("In mPerSec")]
+    [SerializeField] float xRange = 22f;
+    [Tooltip("In mPerSec")]
+    [SerializeField] float yRange = 13f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed* Time.deltaTime;
+
+        float newXPos = transform.localPosition.x + xOffset;
+        float ClampedXPos = Mathf.Clamp(newXPos, -xRange, xRange);
+
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        float ClampedYPos = Mathf.Clamp(newYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(ClampedXPos, ClampedYPos, transform.localPosition.z);
+
+    }
+}
