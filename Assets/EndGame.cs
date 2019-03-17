de0493a4c97c69833655ddfe31269913fa198b8e
S.Vasilev <s.vasilev9169@gmail.com>
2019-03-17 02:10:06 +0200

    Final Commit

---
+++ Assets/EndGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
